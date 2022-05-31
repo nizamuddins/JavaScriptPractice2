46678b2ad8d2590305b8d9749b31ec7f0044644b
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-01 00:17:15 +0530

    program

---
+++ program31 to fund string is pallindrome or not/index1.js
@@ -0,0 +1,23 @@
+function myFunc(str) {
+    const len = str.length;
+
+    for (let i = 0; i < len / 2; i++) {
+
+        if (str[i] !== str[len - 1 - i]) {
+
+            return "It is Not a pallinderome";
+
+        } 
+        
+
+    }
+
+    return "It is pallindrome"
+
+}
+
+const string = prompt("Enter the string ");
+
+const func = myFunc(string);
+
+console.log(func)

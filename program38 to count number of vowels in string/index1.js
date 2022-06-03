05d3bbcb3a45cda59c71b858ae27635a5f482777
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-03 12:50:38 +0530

    program

---
+++ program38 to count number of vowels in string/index1.js
@@ -0,0 +1,27 @@
+const string = prompt("Enter the string");
+
+const vowels = ["a", "e", "i", "o", "u"];
+
+function myFunc(str) {
+
+    let length = 0;
+
+    for (let i = 0; i < str.length; i++) {
+
+        for (let j = 0; j < vowels.length; j++) {
+
+            if (str[i] == vowels[j]) {
+
+                length++;
+                break;
+            }
+
+        }
+
+    }
+
+    return length;
+
+}
+
+console.log(myFunc(string))

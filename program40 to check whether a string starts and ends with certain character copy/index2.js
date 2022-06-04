a635a74a2c04741d0240bdeb6a19bf9cc20b31b7
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-04 19:59:23 +0530

    project

--- program40 to check whether a string starts and ends with certain character copy/index2.js
+++ program40 to check whether a string starts and ends with certain character copy/index2.js
@@ -1,22 +1,16 @@
 const string = prompt("Enter a  string");
-
 function myFunc(str) {
-
     if (/^S/i.test(str) && /G$/i.test(str)) {
-
-        console.log("THe string starts with S and ends with G")
-
+        console.log("THe string startswith S and ends with G ")
     } else if (/^S/i.test(str)) {
         console.log("The string start with S but does not ends with G")
     } else if (/^G/i.test(str)) {
-
-        console.log("The string  does not start with S but ends with G")
-
+        console.log("The string  does not start with Sbut ends with G ")
     } else {
-        console.log("THe string does not starts with S nither ends with G")
-
+        console.log(" THe string does not starts with S nither ends with G ")
     }
-
 }
-
 myFunc(string)
+
+
+

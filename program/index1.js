65c38116c845d9f655b27bf4925df7689259b874
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-08 18:11:29 +0530

    program

--- program/index1.js
+++ program/index1.js
@@ -11,17 +11,49 @@ const child = document.querySelector(".child");
 
 const body = document.body;
 
-// child.addEventListener("click", function () {
-//     console.log("you clicked on child");
-// })
-// parent.addEventListener("click", function () {
-//     console.log("you clicked on parent");
-// })
+// child.addEventListener("click", function () {     console.log("you clicked on
+// child"); }) parent.addEventListener("click", function () { console.log("you
+// clicked on parent"); })
 grand.addEventListener("click", function (e) {
     console.log(e.target.innerText);
 })
 
-// body.addEventListener("click", function () {
-//     console.log("you clicked on body");
-// })
+function myFunc(element) {
+    return new Promise((resolve, reject) => {
+        if (element) {
+            const red = Math.floor(Math.random() * 125);
+            const blue = Math.floor(Math.random() * 125);
+            const green = Math.floor(Math.random() * 125);
+            const random = `rgb(${red},${blue},${green})`;
+            const grand = document.querySelector('.' + element);
+            grand.style.backgroundColor = random;
+            resolve();
+        } else {
+            reject();
+        }
+    })
 
+}
+
+const id1 = setInterval(() => {
+    myFunc("child").then(() => {})
+}, 1000)
+const id2 = setInterval(() => {
+    myFunc("parent").then(() => {})
+}, 1000)
+const id3 = setInterval(() => {
+    myFunc("grand").then(() => {})
+}, 1000)
+
+const grand1 = document.querySelector(".grand");
+grand.addEventListener("click", (e) => {
+    if (e.target.classList.contains("child")) {
+        clearInterval(id1);
+    }
+    if (e.target.classList.contains("parent")) {
+        clearInterval(id2);
+    }
+    if (e.target.classList.contains("grand")) {
+        clearInterval(id3);
+    }
+})

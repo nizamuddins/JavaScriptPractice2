d3a8c6a622f7af76de9eec4a13b57be93c1e3b28
nizamuddins <uddinmdnizam61@gmail.com>
2022-06-30 01:59:21 +0530

    program

--- Program76 to Sort Array of Objects by Property Values/index1.js
+++ Program76 to Sort Array of Objects by Property Values/index1.js
@@ -1,5 +1,4 @@
-const array = [
-
+const arr = [
     {
         name: "nizam",
         age: 21
@@ -13,12 +12,46 @@ const array = [
         name: "nizam",
         age: 13
     }
+] 
+arr.sort((a, b) =>a.age - b.age)
+console.log(arr)
+
+const array1 = [
+    {
+        a: 6
+    }, {
+        a: 6
+    }, {
+        a: 7
+    }, {
+        a: 9
+    }, {
+        a: 8
+    }, {
+        a: 8
+    }, {
+        a: 10
+    }, {
+        a: 10
+    }
 ]
 
-array.sort((a, b) => {
-    return a.age - b.age;
-})
+let array = []
+for (let i = 0; i < array1.length - 1; i++) {
+
+    if (array1[i].a !== array1[i + 1].a) {
+
+        array.push(array1[i + 1])
+
+    } else if (i == 0) {
+        array.push(array1[i])
+    }
+
+}
+
+
+
+
+array.sort((a, b) => b.a - a.a)
+
 console.log(array)
-
-
-

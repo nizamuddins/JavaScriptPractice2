2cf5647ab395fcc05b90995901d9affb983f88a3
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-07 19:14:22 +0530

    program

--- program92 to work with const variable/index1.js
+++ program92 to work with const variable/index1.js
@@ -9,10 +9,13 @@ const a = 5;
 
 console.log(a);
 
-const arr = ['nizam','nayeem'];
+const arr = ['nizam', 'nayeem'];
 
-arr[1]= 'nomaan';
+arr[1] = 'nomaan';
 
 console.log(arr);
 
-
+// const name2 = "level"; const value = myfunc(name2) function myfunc(name1){
+// const length = name1.length; for(let i=0;i<=name1.length-1;i++){
+// if(name1[i]!==name1[length-i-1]) {     return 'not a pallindrome' } } return
+// 'pallindrome string' } console.log(value)

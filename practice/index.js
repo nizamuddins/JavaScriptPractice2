410b7b0673dea7bd80bd4e65932f4fb8f2efb729
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-26 01:10:36 +0530

    project

--- practice/index.js
+++ practice/index.js
@@ -1,4 +1,4 @@
-// // console.log('hello world'); console.log(`hello world`); console.log("hello
+// console.log('hello world'); console.log(`hello world`); console.log("hello
 // world") var a = 2; var b= 6; console.log(a+b); console.log(a*b) var b= 36;
 // console.log(Math.sqrt(b)); area of triangle formula= (base*height)/2 var b=
 // 6; var h = 54; console.log((b*h)/2) var s1= 6; var s2 = 5; var s3 = 2; const
@@ -10,7 +10,17 @@
 // > 0) {     root1 = (-b + (Math.floor(Math.sqrt(deter))) / (2 *
 // a)).toFixed(2);     root2 = (-b - (Math.floor(Math.sqrt(deter))) / (2 *
 // a)).toFixed(2);     console.log(root1, root2) } else if (deter === 0) {
-// root1,     root2 = (-b / 2 * a);     console.log(root1, root2) } else {
-// let imgprt = (-b) / (2 * a)     let realprt =
-// (Math.floor(Math.sqrt(-(deter))) / (2 * a)).toFixed(2)
-// console.log(`${imgprt}+${realprt}i, ${imgprt}-${realprt}i`) }
+// root1,     root2 = (-b / 2 * a);     console.log(root1, root2) } else { let
+// imgprt = (-b) / (2 * a)     let realprt = (Math.floor(Math.sqrt(-(deter))) /
+// (2 * a)).toFixed(2) console.log(`${imgprt}+${realprt}i,
+// ${imgprt}-${realprt}i`) } const kilometers = 24; const factor = 0.62137;
+// const miles = kilometers*factor; console.log(miles.toFixed(2)+" miles") const
+// kilometers2 = miles/factor; console.log(kilometers2 +" kilmeters") const
+// celcius = 20; const fehrenhit = (celcius*1.8)+32; console.log(fehrenhit+ "
+// Feh"); const celcius2 = (fehrenhit-32)/1.8; console.log(celcius2 + " Cel")
+// const num = Math.floor(Math.random()*10)+1; console.log(num) const num = -8;
+// if(num>0){     console.log('Number is Positive') }else if(num ===0){
+// console.log('Number is Zero') }else{     console.log('Number is Negative') }
+// const num = 53; if(num % 2===0){     console.log('Number is Even') }else{
+// console.log('Number is Odd') } const num =4; const value =
+// num%2==0?'Even':"Odd"; console.log(value)
